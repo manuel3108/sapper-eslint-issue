c67189bf36ff6c818230bee21ce1e4ee63f16f7e
Manuel Serret <manuel-serret@t-online.de>
2020-12-29 17:06:51 +0100

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { name } from '../stores/name';
+</script>
+
+<h1>{$name}</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { name } from '../stores/name';
+
+    function onClick() {
+        $name = 2;
+    }
+</script>
+
+<button on:click={onClick}>Some useless html</button>
